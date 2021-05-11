26040b13689613501d9514399eccfa8c6056af9c
zetashift <rskaraya@gmail.com>
2021-05-11 02:27:36 +0200

    Switch parser to proc's because func can't be used in type declarations

--- nimday_21/parser.nim
+++ nimday_21/parser.nim
@@ -1,65 +1,66 @@
 import  
-  std/[strformat, sugar],
+  std/[sequtils, strformat, sugar],
   either
 
 type
   Parser* = ref object
-    ## Parser is an abstraction over our parser function, so we can hold more instructions
-    f: s: string -> Either ## Our actual parser function
-    supressed: bool ## Indicates if we want to ignore the parsed text, for example discarding values in a string
+    ## Parser is an abstraction over our parser proction, so we can hold more instructions
+    f*: string -> Either ## Our actual parser proction
+    supressed*: bool     ## Indicates if we want to ignore the parsed text, for example discarding values in a string
 
-func newParser*(f: (s: string) -> Either, supressed: bool = false): Parser =
+proc newParser*(f: (s: string) -> Either, supressed: bool = false): Parser =
   ## Instantiate a new `Parser object`
-  init result
+  new result
   result.f = f
-  result.surpressed = surpressed
+  result.supressed = supressed
 
-func `$`*(p: Parser): string = fmt"<Parser: >"
+proc `$`*(p: Parser): string = fmt"<Parser: >"
 
-func parse*(this: Parser, s: string): Either = this.f(s)
+proc parse*(this: Parser, s: string): Either = 
+  this.f(s)
 
-func map*(this: Parser, transformer: seq[string] -> seq[string]): Parser =
-  func inner(s: string): Either = this.f(s).map(transformer)
+proc map*(this: Parser, transformer: seq[string] -> seq[string]): Parser =
+  proc inner(s: string): Either = this.f(s).map(transformer)
   result = newParser(inner)
 
-func surpress*(this: Parser): Parser =
+proc surpress*(this: Parser): Parser =
   result = newParser(this.f, true)
 
-func charParser*(c: char): Parser =
+proc charParser*(c: char): Parser =
   ## A Parser that only parses a certain character
-  func curried(s: String): Either =
-    ## This function actually parses the string
+  proc curried(s: string): Either =
+    ## This proction actually parses the string
     if s == "":
       let msg = "String is empty"
       return Either(kind: ekLeft, msg: msg)
     else:
       if s[0] == c:
-        let rem == s[1 ..< s.len]
+        let rem = s[1 ..< s.len]
         let parsedString = @[$c]
         return Either(kind: ekRight, val: (parsed: parsedString, remaining: rem))
       else:
         return Either(kind: ekLeft, msg: &"Expecting '{$c}' but got '{$s[0]}'")
 
-  return newParser(curried)
+  return newParser(f = curried)
 
-func andThen*(p1, p2: Parser): Parser =
+proc andThen*(p1, p2: Parser): Parser =
   ## We try to parse p1 and then p2, if both succeed we return a `Right`.
   ## If one fails we return a `Left`.
-  func curried(s: string): Either =
+  proc curried(s: string): Either =
     let res1 = p1.parse(s)
     case res1.kind
     of ekLeft: return res1
     of ekRight:
-      let res2 = p2.parse(p1.val.remaining) # parse the remaining characters
+      let res2 = p2.parse(res1.val.remaining) # parse the remaining characters
       case res2.kind
       of ekLeft: return res2
       of ekRight:
         let v1 = res1.val.parsed
         let v2 = res2.val.parsed
         var vs: seq[string] = @[]
-        if not p1.surpressed:
+        if not p1.supressed:
           vs.add(v1)
-        if not p2.surpressed:
+        if not p2.supressed:
           vs.add(v2)
         return Either(kind: ekRight, val: (parsed: vs, remaining: res2.val.remaining))
       else:
@@ -67,14 +68,14 @@ func andThen*(p1, p2: Parser): Parser =
   
   result = newParser(curried)
 
-func `>>`*(left: Parser, right: Parser): Parser = 
+proc `>>`*(left: Parser, right: Parser): Parser = 
   ## A wrapper around `andThen` for chaining parsers
   result = andThen(left, right)
 
-func orElse*(p1, p2: Parser): Parser =
+proc orElse*(p1, p2: Parser): Parser =
   ## We try to parse1 and then pars2, if atleast one succeed we return a `Right`.
   ## If both fail, that's the only time we return a `Left`.
-  func curried(s: string): Either =
+  proc curried(s: string): Either =
     let res = p1.parse(s)
     case res.kind
     of ekRight: return res  ## If the first one succeeds we can immediately return, just like a boolean `or` operator!
@@ -88,8 +89,37 @@ func orElse*(p1, p2: Parser): Parser =
 
   result = newParser(curried)
 
-func `|`*(left: Parser, right: Parser): Parser =
+proc `|`*(left: Parser, right: Parser): Parser =
   ## A wrapper around `orElse`, where we try to parse one or the other
   result = orElse(left, right)
 
+proc multiply*(parser: Parser, count: int): Parser =
+  ## Here we want to parse something `count` amount of times
+  proc curried(s: string): Either =
+    var copyS = s
+    var fullParsed: seq[string] = @[]
+    for i in 1..count:
+      let res = parser.parse(copyS)
+      case res.kind
+      of ekLeft: return res
+      of ekRight:
+        let parsed = res.val.parsed
+        copyS = res.val.remaining
+        fullparsed.add(parsed)
 
+    return Either(kind: ekRight, val: (parsed: fullparsed, remaining: copyS))
+
+  result = newParser(curried)
+
+proc `*`*(p: Parser, times: int): Parser =
+  multiply(p, times)
+
+proc choice*(parsers: seq[Parser]): Parser =
+  ## Try to run a sequence of Parsers
+  result = parsers.foldl(a | b)
+
+proc anyOf*(chars: set[char]): Parser =
+  ## converts a sequence of characters into a `charParser`
+  result = chars.mapIt(charParser(it)).choice
+
+proc parseString(str: string): Parser = discard
